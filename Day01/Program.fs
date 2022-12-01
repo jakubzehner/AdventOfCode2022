a20dc3dfe6faa0ce270312d00a170df1c1524a5c
Jakub Zehner <kubusja.pl@gmail.com>
2022-12-01 12:40:18 +0100

    Add day 1 part 1

---
+++ Day01/Program.fs
@@ -0,0 +1,18 @@
+﻿let input = "input.txt"
+
+let readLines filePath =
+    System.IO.File.ReadLines(filePath)
+
+let findMax filePath =
+    let rec findMaxRec current maxNumber lines =
+        match lines with
+        | [] -> maxNumber
+        | ""::t -> findMaxRec 0 (max current maxNumber) t
+        | number::t -> findMaxRec (current + (number |> int)) maxNumber t
+
+    findMaxRec 0 0 (List.ofSeq (readLines filePath))
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%d" (findMax input)
+    0
