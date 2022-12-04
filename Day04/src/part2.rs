3aa2cc587960dc67a88819aba7e60b6eba2f152c
Jakub Zehner <kubusja.pl@gmail.com>
2022-12-04 15:39:54 +0100

    fix function name

--- Day04/src/part2.rs
+++ Day04/src/part2.rs
@@ -1,7 +1,7 @@
 pub fn calculate_containments(input: &&Vec<Vec<u32>>) -> usize {
-    input.iter().filter(check_for_full_containment).count()
+    input.iter().filter(check_for_containment).count()
 }
 
-fn check_for_full_containment(pair: &&Vec<u32>) -> bool {
+fn check_for_containment(pair: &&Vec<u32>) -> bool {
     pair[0] <= pair[3] && pair[1] >= pair[2]
 }
